2d4294d5f8650ddb1fd8b98807e82f7afec46cb6
TXzzera <brunoteixeira.10@hotmail.com>
2026-02-10 01:33:44 -0300

    att week06

--- art-crafted-for-you/src/app/login/page.tsx
+++ art-crafted-for-you/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { signIn } from "next-auth/react";
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 
 export default function LoginPage() {
   return (
